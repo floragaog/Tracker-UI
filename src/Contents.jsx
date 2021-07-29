ff5e3a706e0048338da5e6dc1350a89733c58ef7
Ge <gaoge2128@163.com>
2021-07-29 16:10:26 -0700

    First commit

---
+++ src/Contents.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import routes from './routes.js';
+
+const NotFound = () => <h1>Page Not Found</h1>;
+
+export default function Contents() {
+  return (
+    <Switch>
+      <Redirect exact from="/" to="/issues" />
+      {routes.map(attrs => <Route {...attrs} key={attrs.path} />)}
+
+    </Switch>
+  );
+}
